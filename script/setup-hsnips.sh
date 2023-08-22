9041391625cc90721973cdd01ee08fa00b6ecc4e
shena <87386937+Shena4746@users.noreply.github.com>
2023-08-23 02:15:44 +0900

    changed: update snippets and commands incorporating those in my other repository for snippets.

--- script/setup-hsnips.sh
+++ script/setup-hsnips.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # locate latex.hsnips at script directory
+workspaceFolder=$(readlink -f $(dirname $(dirname $0)))
 file='latex.hsnips'
-if [ ! -f ./.vscode/${file} ]; then
-    wget -P ./.vscode https://raw.githubusercontent.com/Shena4746/LaTeX-Snippets-for-VSCode/main/${file}
+if [ ! -f ${workspaceFolder}/.vscode/${file} ]; then
+    wget -P ./.vscode https://raw.githubusercontent.com/Shena4746/latex-snippets-for-vscode/main/.vscode/${file}
 fi
